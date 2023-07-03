245576bf6ec226e2553c2d64217d2f93e4ea4c3f
liuqi6908 <liuqi6908@163.com>
2023-07-03 10:05:32 +0800

    添加ssl证书

--- server.js
+++ server.js
@@ -18,17 +18,34 @@
  */
 
 var express = require("express");
-var http = require("http");
+/* var http = require("http"); */
+var https = require("https");
+
+// 读取 ssl 证书内人
+var fs = require("fs");
+var path = require("path");
+var ssl = {
+  key: fs.readFileSync(path.join(__dirname, "/ssl/rdpweb.qiyandata.com.key")),
+  cert: fs.readFileSync(path.join(__dirname, "/ssl/rdpweb.qiyandata.com.pem")),
+};
 
 var app = express();
 app.use(express.static(__dirname + "/client"));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/client/html/index.html");
 });
-var server = http
+/* var server = http
   .createServer(app)
   .listen(process.env.PORT || 9250, function () {
     console.log("远程桌面连接 监听端口 =====> " + (process.env.PORT || 9250));
+  }); */
+// 创建 https 服务
+var server = https
+  .createServer(ssl, app)
+  .listen(process.env.PORT || 9250, function () {
+    console.log(
+      "远程桌面连接 监听端口 =====> " + (process.env.PORT || 9250) + "(https)"
+    );
   });
 
 require("./server/mstsc")(server);
